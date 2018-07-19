c1ec9d736e58afd7c4bc176db959f78b50257ff3
alexg622 <alex.gm62288@gmail.com>
2018-07-18 22:45:36 -0700

    Make user routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,4 +3,31 @@ const router = express.Router()
 
 router.get("/test", (req, res) => res.json({msg: "working"}))
 
+router.get('/', (req, res) => {
+  User.find().then(users => res.json(users))
+})
+
+router.get('/:userId', (req, res) => {
+  User.findById(req.params.userId).then(user => res.json(user))
+})
+
+router.post('/', (req, res) => {
+  const newUser = new User({
+    name: req.body.name,
+    password: req.body.password
+  })
+  newUser.save().then(user => res.json(user))
+})
+
+router.patch('/:userId', (req, res) => {
+  User.update({_id: req.params.userId}, {name: req.body.name, password: req.body.password})
+    .then(user => res.json(user))
+})
+
+router.delete('/:userId', (req, res) => {
+  User.findById(req.params.userId).then(user => {
+    user.remove().then(user => res.json(user))
+  })
+})
+
 module.exports = router
